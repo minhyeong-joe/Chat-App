d68c10d95ee1f6c0578beffab0c74cf510c08550
minhyeong-joe <minhyeong.billy.joe@gmail.com>
2018-08-08 20:44:05 -0700

    heroku deploy

--- api/auth.php
+++ api/auth.php
@@ -1,6 +1,11 @@
 <?php
   // Header
-  header('Access-Control-Allow-Origin: http://localhost:3000, https://itok-chat-app.heroku.app.com');
+  $http_origin = $_SERVER['HTTP_ORIGIN'];
+
+  if ($http_origin == "http://localhost:3000" || $http_origin == "https://itok-chat-app.heroku.app.com")
+  {
+    header("Access-Control-Allow-Origin: $http_origin");
+  }
   header('Content-Type: application/json');
   header('Access-Control-Allow-Methods: GET, POST');
   header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
